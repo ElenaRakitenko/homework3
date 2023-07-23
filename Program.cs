25275862ad9d148e9413622460e190e8227d5113
Elena <elenarakitenko@yandex.ru>
2023-07-23 22:04:50 +0300

    задача 19

---
+++ Program.cs
@@ -0,0 +1,166 @@
+﻿Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Задача 19");
+Console.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом");
+
+// запрашивание ввода данных
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if ((number < 10000) && (number > 100000))
+{
+    Console.WriteLine("Число введено неверно, повторите ввод");
+}
+else if (number = number/10000) == (number = number % 10) && (number % 10000) == (number % 100))
+{
+  
+}
+
+// {
+//     (number = number/10000) == (number = number % 10) && (number % 10000) == (number % 100))
+
+//     Console.WriteLine("Да");
+// }
+// else 
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Задача 18
+
+// Console.WriteLine("Введите номер четверти");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == 1)
+// {
+//     Console.WriteLine("X>0 и Y>0");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("X<0 и Y>0");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("X<0 и Y<0");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("X>0 и Y<0");
+// }
+// else if(number > 4)
+// {
+//     Console.WriteLine("Номер четверти введен неверно,повторите ввод");
+// }
+
+
+// Задача 21
+
+// Console.WriteLine("Введите координату X1");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y1");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату X2");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y2");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt((Math.Pow((X2-X1),2)) + (Math.Pow((Y2-Y1),2)));
+// Console.WriteLine($"Расстояние между двумя точками {result}");
+
+
+// Задача 22
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i <=num; i++)
+// {
+//     Console.WriteLine(Math.Pow(i,2));
+// } 
+//  Задача 17
+
+// Console.WriteLine("Введите X");
+// int X = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите Y");
+// int Y = Convert.ToInt32(Console.ReadLine());
+
+// if ((X > 0) && (Y > 0))
+// {
+//     Console.WriteLine("1 Четверть");
+// }
+// else if ((X<0) && (Y>0))
+// {
+//     Console.WriteLine("2 Четверть");
+// }
+// else if ((X<0) && (Y<0))
+// {
+//     Console.WriteLine("3 Четверть");
+// }
+// else if ((X>0) && (Y<0))
+// {
+//     Console.WriteLine("4 Четверть");
+// }
+
+
+// Задача 18
+
+// Console.WriteLine("Введите номер четверти");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == 1)
+// {
+//     Console.WriteLine("X>0 и Y>0");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("X<0 и Y>0");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("X<0 и Y<0");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("X>0 и Y<0");
+// }
+// else if(number > 4)
+// {
+//     Console.WriteLine("Номер четверти введен неверно,повторите ввод");
+// }
+
+
+// Задача 21
+
+// Console.WriteLine("Введите координату X1");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y1");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату X2");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y2");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt((Math.Pow((X2-X1),2)) + (Math.Pow((Y2-Y1),2)));
+// Console.WriteLine($"Расстояние между двумя точками {result}");
+
+
+// Задача 22
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i <=num; i++)
+// {
+//     Console.WriteLine(Math.Pow(i,2));
+// } 
